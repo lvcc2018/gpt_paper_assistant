653a4078ead2362c094471f3717c490cabdd8b90
Claire Lv <thulvcc2017@gmail.com>
2024-02-23 13:55:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -181,11 +181,12 @@ if __name__ == "__main__":
 
     S2_API_KEY = os.environ.get("S2_KEY")
     OAI_KEY = os.environ.get("OAI_KEY")
-    if OAI_KEY is None:
+    BASE_URL = os.environ.get("OAI_BASE_URL")
+    if OAI_KEY is None or BASE_URL is None:
         raise ValueError(
             "OpenAI key is not set - please set OAI_KEY to your OpenAI key"
         )
-    openai_client = OpenAI(api_key=OAI_KEY)
+    openai_client = OpenAI(api_key=OAI_KEY, base_url=BASE_URL)
     # load the author list
     with io.open("configs/authors.txt", "r") as fopen:
         author_names, author_ids = parse_authors(fopen.readlines())
